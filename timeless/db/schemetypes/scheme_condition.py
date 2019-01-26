65ecaf18790766f33821ea61ff900d49ce98bba9
amihaiemil <amihaiemil@gmail.com>
2019-01-26 16:40:53 +0200

    #18 rename table and removed onupdate

--- timeless/db/schemetypes/scheme_condition.py
+++ timeless/db/schemetypes/scheme_condition.py
@@ -10,14 +10,14 @@ class SchemeCondition(DB.Model):
      still need to be added -- we'll probably need additional tables to represent these
      relations.
     """
-    __tablename__ = "schemeconditions"
+    __tablename__ = "scheme_conditions"
 
     id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
 
     value = DB.Column(DB.String, unique=True, nullable=False)
     priority = DB.Column(DB.Integer, nullable=False)
     start_time = DB.Column(DB.DateTime, default=datetime.utcnow, nullable=False)
-    end_time = DB.Column(DB.DateTime, onupdate=datetime.utcnow, nullable=False)
+    end_time = DB.Column(DB.DateTime, nullable=False)
 
     def __repr__(self):
         return "<SchemeCondition %r>" % self.id
