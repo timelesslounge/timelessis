5799d7776b654e1b70e68382a79839cf0c30365c
amihaiemil <amihaiemil@gmail.com>
2019-01-29 11:15:11 +0200

    #17 puzzle length

--- timeless/schemetypes/models.py
+++ timeless/schemetypes/models.py
@@ -26,7 +26,9 @@ class SchemeCondition(DB.Model):
 
 class SchemeType(DB.Model):
     """
-    @todo #17:30min Continue implementation as in #17.
+    @todo #17:30min Continue implementation as in #17. SchemeType should have
+     its own management pages to list, create, edit and delete them. In the
+     index page it should be possible to sort and filter for every column.
     """
     __tablename__ = "scheme_types"
 
