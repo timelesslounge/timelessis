add7557f89229884e1cd42546cb0a145701cdd8c
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-01-29 14:27:56 +0300

    #32: change route name

--- timeless/reservations/views.py
+++ timeless/reservations/views.py
@@ -4,9 +4,9 @@ from flask import Blueprint
 bp = Blueprint('reservations', __name__, url_prefix='/reservations')
 
 
-@bp.route('/settings_page')
+@bp.route('/settings')
 def base():
     """
     @todo #32:30min Continue implementing Settings page
     """
-    return 'Settings page'
+    return 'Settings API entry point'
