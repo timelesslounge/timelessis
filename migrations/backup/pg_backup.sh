3211153fa82469980f16e694ed0aaf348c429acf
paulodamaso <pauloeduardolobo@gmail.com>
2019-01-29 10:28:44 -0200

    For #47: Review corrections

--- migrations/backup/pg_backup.sh
+++ migrations/backup/pg_backup.sh
@@ -14,26 +14,26 @@
 #  pg_backup.sh to be executed in linux cron. The frequency of execution must be
 #  defined by the architect and put in the tickets comments.
 while [ $# -gt 0 ]; do
-        case $1 in
-                -c)
-                        if [ -r "$2" ]; then
-                                source "$2"
-                                shift 2
-                        else
-                                ${ECHO} "Unreadable config file \"$2\"" 1>&2
-                                exit 1
-                        fi
-                        ;;
-                *)
-                        ${ECHO} "Unknown Option \"$1\"" 1>&2
-                        exit 2
-                        ;;
-        esac
+    case $1 in
+        -c)
+            if [ -r "$2" ]; then
+                source "$2"
+                shift 2
+            else
+                echo "[ERROR]["date +\%Y-\%m-\%d"] Unreadable config file \"$2\"" 1>&2
+                exit 1
+            fi
+            ;;
+        *)
+            echo " [ERROR]["date +\%Y-\%m-\%d"] Unknown Option \"$1\"" 1>&2
+            exit 2
+            ;;
+    esac
 done
 
 if [ $# = 0 ]; then
-        SCRIPTPATH=$(cd ${0%/*} && pwd -P)
-        source $SCRIPTPATH/pg_backup.config
+    SCRIPTPATH=$(cd ${0%/*} && pwd -P)
+    source $SCRIPTPATH/pg_backup.config
 fi;
 
 ###########################
@@ -42,7 +42,7 @@ fi;
 
 # Make sure we're running as the required backup user
 if [ "$BACKUP_USER" != "" -a "$(id -un)" != "$BACKUP_USER" ]; then
-	echo "This script must be run as $BACKUP_USER. Exiting." 1>&2
+	echo "[ERROR]["date +\%Y-\%m-\%d"] This script must be run as $BACKUP_USER. Exiting." 1>&2
 	exit 1;
 fi;
 
@@ -70,7 +70,7 @@ FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`/"
 echo "Making backup directory in $FINAL_BACKUP_DIR"
 
 if ! mkdir -p $FINAL_BACKUP_DIR; then
-	echo "Cannot create backup directory in $FINAL_BACKUP_DIR. Go and fix it!" 1>&2
+	echo "[ERROR]["date +\%Y-\%m-\%d"] Cannot create backup directory in $FINAL_BACKUP_DIR. Go and fix it!" 1>&2
 	exit 1;
 fi;
 
@@ -84,9 +84,9 @@ echo -e "--------------------------------------------\n"
 echo "Plain backup of $DATABASE"
 
 if ! pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" "$DATABASE" | gzip > $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress; then
-    echo "[!!ERROR!!] Failed to produce plain backup database $DATABASE" 1>&2
+	echo "[ERROR]["date +\%Y-\%m-\%d"] Failed to produce plain backup database $DATABASE" 1>&2
 else
-    mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
+	mv $FINAL_BACKUP_DIR"$DATABASE".sql.gz.in_progress $FINAL_BACKUP_DIR"$DATABASE".sql.gz
 fi
 
 echo -e "\nAll database backups complete!"
