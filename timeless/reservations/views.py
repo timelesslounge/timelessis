9f494674f496e55bb5d0d5a530bbc170804360af
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-01-29 18:51:34 +0300

    #32: extend puzzle message

--- timeless/reservations/views.py
+++ timeless/reservations/views.py
@@ -7,6 +7,8 @@ bp = Blueprint('reservations', __name__, url_prefix='/reservations')
 @bp.route('/settings')
 def base():
     """
-    @todo #32:30min Continue implementing Settings page for Reservations, it will be deployed on a different subdomain
+    @todo #32:30min Continue implementing Settings page for Reservations,
+    it will be deployed on a different subdomain. Page should have set of
+    fields from ReservationSettings model.
     """
     return 'Settings API entry point'
