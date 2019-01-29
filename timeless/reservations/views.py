d8c29b28300b828857c2db7c8a4b9f9dccdb483f
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-01-29 18:32:18 +0300

    #32: change puzzle description

--- timeless/reservations/views.py
+++ timeless/reservations/views.py
@@ -7,6 +7,6 @@ bp = Blueprint('reservations', __name__, url_prefix='/reservations')
 @bp.route('/settings')
 def base():
     """
-    @todo #32:30min Continue implementing Settings page
+    @todo #32:30min Continue implementing Settings page for Reservations, it will be deployed on a different subdomain
     """
     return 'Settings API entry point'
