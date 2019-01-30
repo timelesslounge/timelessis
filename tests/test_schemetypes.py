1c67541db3f8c3e193e374370bde781dce699ffc
Boris Kuzmic <boris.kuzmic@gmail.com>
2019-01-30 22:49:21 +0100

    #43 Adding weekdays, monthdays and dates for SchemeCondition

--- tests/test_schemetypes.py
+++ tests/test_schemetypes.py
@@ -1,6 +1,6 @@
 """Tests for models in the schemetypes package."""
-from timeless.schemetypes.models import SchemeCondition, SchemeType
-
+from timeless.schemetypes.models import SchemeCondition, SchemeType, WeekDay, MonthDay, Date
+from datetime import datetime
 
 def test_new_scheme_condition():
     condition = SchemeCondition(id=1, value="test", priority=2)
@@ -15,3 +15,23 @@ def test_new_scheme_type():
             and scheme_type.description == "description"
             and scheme_type.default_value == "2"
             and scheme_type.value_type == "STRING")
+
+def test_new_weekday():
+    day = WeekDay(id=1, weekday=1, scheme_condition_id=2)
+    assert (day.id == 1
+            and day.weekday == 1
+            and day.scheme_condition_id == 2)
+
+def test_new_monthday():
+    day = MonthDay(id=1, monthday=1, scheme_condition_id=2)
+    assert (day.id == 1
+            and day.monthday == 1
+            and day.scheme_condition_id == 2)    
+
+def test_new_date():
+    month_year = "01-2019"
+    date_format = "%m-%Y"
+    test_date = Date(id=1, date=datetime.strptime(month_year, date_format), scheme_condition_id=2)
+    assert (test_date.id == 1
+            and test_date.date == datetime.strptime(month_year, date_format)
+            and test_date.scheme_condition_id == 2)                       
--- timeless/schemetypes/models.py
+++ timeless/schemetypes/models.py
@@ -1,13 +1,40 @@
 from timeless.db import DB
 from datetime import datetime
 
+class WeekDay(DB.Model):
+    __tablename__ = "weekdays"
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    weekday = DB.Column(DB.Integer, unique=True, nullable=False)
+    scheme_condition_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_conditions.id"))
+
+    scheme_condition = DB.relationship("SchemeCondition", back_populates="weekdays")
+
+    def __repr__(self):
+        return "<Weekday %r>" % self.id
+
+class MonthDay(DB.Model):
+    __tablename__ = "monthdays"
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    monthday = DB.Column(DB.Integer, unique=True, nullable=False)
+    scheme_condition_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_conditions.id"))
+
+    scheme_condition = DB.relationship("SchemeCondition", back_populates="monthdays")
+
+    def __repr__(self):
+        return "<MonthDay %r>" % self.id     
+
+class Date(DB.Model):
+    __tablename__ = "dates"
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    date = DB.Column(DB.DateTime, unique=True, nullable=False)
+    scheme_condition_id = DB.Column(DB.Integer, DB.ForeignKey("scheme_conditions.id"))
+
+    scheme_condition = DB.relationship("SchemeCondition", back_populates="dates")
+
+    def __repr__(self):
+        return "<Date %r>" % self.id        
 
 class SchemeCondition(DB.Model):
-    """
-    @todo #18:30min Continue implementation as in #18. Weekdays, months and dates
-     still need to be added -- we'll probably need additional tables to represent these
-     relations.
-    """
     __tablename__ = "scheme_conditions"
 
     id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
@@ -17,6 +44,9 @@ class SchemeCondition(DB.Model):
     priority = DB.Column(DB.Integer, nullable=False)
     start_time = DB.Column(DB.DateTime, default=datetime.utcnow, nullable=False)
     end_time = DB.Column(DB.DateTime, nullable=False)
+    weekdays = DB.relationship("WeekDay", order_by=WeekDay.id, back_populates="scheme_condition")
+    monthdays = DB.relationship("MonthDay", order_by=MonthDay.id, back_populates="scheme_condition")
+    dates = DB.relationship("Date", order_by=Date.id, back_populates="scheme_condition")
 
     scheme_type = DB.relationship("SchemeType", back_populates="conditions")
 
