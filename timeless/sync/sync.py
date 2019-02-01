a9d3bfb43818dc0d1215c03c3d1df103528aecca
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-01 00:09:57 -0200

    For #66: Puzzle fix

--- timeless/sync/sync.py
+++ timeless/sync/sync.py
@@ -3,7 +3,8 @@
     @todo #66:30min Implement synchronization between Poster and Databse for
      Locations. Data coming from Poster has priority upon data stored in our
      database. After implementing this, remove @unittest.skip annotation from
-     test_sync_location in test_sync.py
+     test_sync_location in test_sync.py. Do not forget to create cron script and
+     put int into scripts folder to make this sync periodic.
 
 """
 class PosterSync():
