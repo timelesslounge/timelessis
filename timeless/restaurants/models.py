4507fe2e88312571c7b8151821a88c7d63e65c60
Vatavuk <vatavuk1@hotmail.com>
2019-02-04 21:57:11 +0100

    #27 Reservation model

--- timeless/restaurants/models.py
+++ timeless/restaurants/models.py
@@ -1,7 +1,19 @@
 """File for models in restaurants module"""
+import enum
+
 from timeless.db import DB
+from timeless.models import TimestampsMixin
 from timeless.poster.models import PosterSyncMixin
 
+class ReservationStatus(enum.Enum):
+    """Reservation status"""
+    unconfirmed = 1
+    confirmed = 2
+    started = 3
+    finished = 4
+    canceled = 5
+    late = 6
+    not_contacting = 7
 
 class TableShape(DB.Model):
     """Model for a Table's Shape.
@@ -92,3 +104,41 @@ class Table(DB.Model):
 
     def __repr__(self):
         return "<Table %r>" % self.name
+
+
+class TableReservation(DB.Model):
+    """Association table for reservations and tables"""
+
+    __tablename__ = 'table_reservation'
+
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    reservation_id = DB.Column(DB.Integer, DB.ForeignKey('reservations.id'))
+    table_id = DB.Column(DB.Integer, DB.ForeignKey('tables.id'))
+    tables = DB.relationship("Table")
+
+
+class Reservation(TimestampsMixin, DB.Model):
+    """Model for a Reservation
+    @todo #27:30min Calculate duration in constructor using start_time and end_time, don't forget to call super
+     constructor. Then continue implementation of views. Index and a view page should be created to list all
+     reservations. In the index page there should be also a function to delete the reservation
+     (after confirmation). In the index page it should be possible to sort and filter for every column.
+    """
+
+    __tablename__ = "reservations"
+
+    id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    start_time = DB.Column(DB.DateTime, nullable=False)
+    end_time = DB.Column(DB.DateTime, nullable=False)
+    duration = DB.Column(DB.Time, nullable=False)
+    customer_id = DB.Column(DB.Integer, DB.ForeignKey("customers.id"))
+    num_of_persons = DB.Column(DB.DateTime, nullable=False)
+    comment = DB.Column(DB.String, nullable=False)
+    status = DB.Column(DB.Enum(ReservationStatus), nullable=False)
+
+    tables = DB.relationship("TableReservation")
+
+    def __repr__(self):
+        return "<Reservation %r>" % self.id
+
+
