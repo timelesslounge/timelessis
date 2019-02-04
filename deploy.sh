46566b1e6d3206bf6f1a77e2a3bccf81c3771b40
Boris Kuzmic <boris.kuzmic@gmail.com>
2019-02-04 22:00:35 +0100

    #45 Adding echo for logging purposes

--- deploy.sh
+++ deploy.sh
@@ -6,21 +6,19 @@
 #  Add scripts in cron (like the one created in #47). Verify the web 
 #  application is running (execute couple fo curl requests).
 
-# run tests
+echo "-- Run tests"
 pytest
 
-# create a tag
-git tag -a $tag -m "Rultor deploy version $tag"
+echo "-- Creating staging tag"
+git tag -a staging-$tag -m "Rultor deploy staging-$tag"
 
-# copy code on remote server
+echo "-- Copy application code to staging server"
 scp -r . user@staging-server:/app
 
-# create database (if not existing)
-# run migrations
-# set env variables and restart the server
 # add scripts in cron (like the one created in #47)
 # verify the webapplication is running
 ssh user@staging-server << EOF
+  echo "-- Creating database user: timless_user"
   sudo -u postgres psql -c "CREATE USER timeless_user WITH 
     SUPERUSER
     CREATEDB
@@ -28,13 +26,19 @@ ssh user@staging-server << EOF
     INHERIT
     LOGIN
     ENCRYPTED PASSWORD 'timeless_pwd';"
+  echo "-- Creating database: timelessdb_dev"  
   sudo -u postgres psql -c "CREATE DATABASE timelessdb_dev;"
+  echo "-- Creating database: timelessdb_test"  
   sudo -u postgres psql -c "CREATE DATABASE timelessdb_test;"
+  echo "-- TODO: add scripts in cron"
   cd /app
+  echo "-- Running database migrations"
   python manage.py db upgrade
+  echo "-- Running web application server"
   export FLASK_APP=main.py
   export FLASK_ENV=development
-  flask run
+  flask run &
+  echo "-- TODO: verify web application is running ok"
 EOF
 
 
