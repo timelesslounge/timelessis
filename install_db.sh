0c3bdaa1c820ca5bc5d64105529cc8c02a72d9ea
Emiliano Della Casa <emiliano.dellacasa@gmail.com>
2019-02-05 10:43:43 +0100

    #37 Add file

---
+++ install_db.sh
@@ -0,0 +1,15 @@
+# Scripts to install Postgres and init timelessis databases
+echo "Start Postgres server"
+/etc/init.d/postgresql start
+echo "Creating user: timeless_user"
+sudo -u postgres psql -c "CREATE USER timeless_user WITH 
+    SUPERUSER
+    CREATEDB
+    CREATEROLE
+    INHERIT
+    LOGIN
+    ENCRYPTED PASSWORD 'timeless_pwd';"
+echo "Creating database: timelessdb_dev"
+sudo -u postgres psql -c "CREATE DATABASE timelessdb_dev;"
+echo "Creating database: timelessdb_test"
+sudo -u postgres psql -c "CREATE DATABASE timelessdb_test;"
