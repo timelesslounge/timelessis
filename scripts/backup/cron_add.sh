07a900e32eb8221acbc845a367e28f1a6d9fa84a
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-05 10:48:26 -0200

    For 116: Added cli parameter to cron script.

--- scripts/backup/cron_add.sh
+++ scripts/backup/cron_add.sh
@@ -2,6 +2,11 @@
 #
 # Create a backup script and set it into cron.
 #
-echo '* * * * * ./pg_backup.sh' > backup.sh
-chmod 755 backup.sh
-sudo mv backup.sh /etc/cron.d/
+
+if [ "$1" == "" ]; then
+    echo "Please provide a cron expression as script parameter in the format \"* * * * *\""
+else
+    echo  $1 " ./pg_backup.sh" > backup.sh
+    chmod 755 backup.sh
+    sudo mv backup.sh /etc/cron.d/
+fi
