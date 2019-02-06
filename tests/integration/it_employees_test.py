7f17b9ec9c1965d56531b1de718c6f99e5a69b62
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-02-06 15:59:31 +0100

    #103 created test for TimestampsMixin

--- tests/integration/it_employees_test.py
+++ tests/integration/it_employees_test.py
@@ -15,3 +15,28 @@ def test_insert_employee(db_session):
     db_session.commit()
     row = db_session.query(Employee).get(employee.id)
     assert row.username == "alice"
+
+
+def test_timestamp_mixin(db_session):
+    """Integration test for testing TimestampsMixin"""
+    before = datetime.utcnow()
+    employee = Employee(
+        first_name="John", last_name="Smith",
+        username="john", phone_number="123", birth_date=datetime.utcnow(),
+        registration_date=datetime.utcnow(), account_status="A",
+        user_status="Working", email="test@test.com", password="bla",
+        pin_code=1234
+    )
+    db_session.add(employee)
+    db_session.commit()
+    row = db_session.query(Employee).get(employee.id)
+    assert row.created_on > before
+    assert row.updated_on > before
+    old_created_on = row.created_on
+    old_updated_on = row.updated_on
+    employee.email = "test@example.com"
+    db_session.merge(employee)
+    db_session.commit()
+    row = db_session.query(Employee).get(employee.id)
+    assert row.created_on == old_created_on
+    assert row.updated_on > old_updated_on
--- timeless/models.py
+++ timeless/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from timeless import DB
 
+
 class TimestampsMixin(object):
     """Mixin for adding created_on and updated_on attributes
      to any models that need to keep track of their updates.
@@ -9,4 +10,7 @@ class TimestampsMixin(object):
      updated.
     """
     created_on = DB.Column(DB.DateTime, default=datetime.utcnow, nullable=False)
-    updated_on = DB.Column(DB.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    updated_on = DB.Column(
+        DB.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow,
+        nullable=False
+    )
