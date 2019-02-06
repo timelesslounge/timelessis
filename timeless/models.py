1d7f9d1c4767ef22d70ea624e2938d92ccd7c986
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-02-06 16:01:00 +0100

    #103 removed todo

--- timeless/models.py
+++ timeless/models.py
@@ -5,9 +5,6 @@ from timeless import DB
 class TimestampsMixin(object):
     """Mixin for adding created_on and updated_on attributes
      to any models that need to keep track of their updates.
-    @todo #40:30min Create a unit test for TimestampsMixin. We need to check if
-     created_on field is correctly populated, and updated_on field is correctly
-     updated.
     """
     created_on = DB.Column(DB.DateTime, default=datetime.utcnow, nullable=False)
     updated_on = DB.Column(
