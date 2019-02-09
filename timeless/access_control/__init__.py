3413994fac28c95012f025da8032cb4862c59348
Vatavuk <vatavuk1@hotmail.com>
2019-02-09 13:30:02 +0100

    #22 fixed puzzles

--- timeless/access_control/__init__.py
+++ timeless/access_control/__init__.py
@@ -1,16 +1,26 @@
 """
-@todo #22:30min Implement Administrator access control.
+@todo #22:30min Implement Administrator access control by
+ implementing privileges logic and including it to the
+ authorization functionality. See Issue #22 for more details about
+ his privileges.
+@todo #22:30min Implement Director access control by
+ implementing privileges logic and including it to the
+ authorization functionality. See Issue #22 for more details about
+ his privileges.
+@todo #22:30min Implement Manager access control by implementing privileges
+ logic and including it to the authorization functionality.
  See Issue #22 for more details about his privileges.
-@todo #22:30min Implement Director access control.
- See Issue #22 for more details about his privileges.
-@todo #22:30min Implement Manager access control.
- See Issue #22 for more details about his privileges.
-@todo #22:30min Implement Location Admin access control.
- See Issue #22 for more details about his privileges.
-@todo #22:30min Implement Master / Intern /Others access control.
- See Issue #22 for more details about his privileges.
-@todo #22:30min Add SecuredView to all views that need access
- control. Create IT tests to verify the security flow.
+@todo #22:30min Implement Location Admin access control by
+ implementing privileges logic and including it to the
+ authorization functionality. See Issue #22 for more details about
+ his privileges.
+@todo #22:30min Implement Master/Intern/Others access control by
+ implementing privileges logic and including it to the
+ authorization functionality. See Issue #22 for more details about
+ his privileges.
+@todo #22:30min Add SecuredView, with accompanied resource type,
+ to all views that need access control. Create IT tests to verify the
+ security flow.
 """
 
 
