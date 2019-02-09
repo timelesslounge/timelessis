760ac882d33b840b653adb03cd2dff9fd19fe8fe
Vatavuk <vatavuk1@hotmail.com>
2019-02-09 17:43:39 +0100

    #59 rev comments

--- tests/integration/it_test_auth.py
+++ tests/integration/it_test_auth.py
@@ -1,12 +1,15 @@
+from http import HTTPStatus
+
+
 def test_login(client, auth):
-    assert client.get("/auth/login").status_code == 200
+    assert client.get("/auth/login").status_code == HTTPStatus.OK
     response = auth.login()
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
 
 
 def test_forgot_password(client):
-    assert client.get("/auth/forgotpassword").status_code == 200
+    assert client.get("/auth/forgotpassword").status_code == HTTPStatus.OK
 
 
 def test_activate(client):
-    assert client.get("/auth/activate").status_code == 405
+    assert client.get("/auth/activate").status_code == HTTPStatus.METHOD_NOT_ALLOWED
