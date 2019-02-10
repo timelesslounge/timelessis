d7d48d7becc73ea0615c179b2e8557682d585aed
Vatavuk <vatavuk1@hotmail.com>
2019-02-10 18:24:02 +0100

    #59 auth tests fixed

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,43 +1,38 @@
 from datetime import datetime
 
-from timeless import DB
 from timeless.auth.auth import login
 from timeless.employees.models import Employee
 
 
-def test_incorrect_username(app):
-    with app.app_context():
-        assert (login("unknown", "unknown") == "Incorrect username.")
+def test_incorrect_username(db_session):
+    """Do not remove db_session dependency. It is injected
+    in order to trigger db migration.
+    """
+    assert (login("unknown", "unknown") == "Incorrect username.")
 
 
-def test_incorrect_password(app):
-    with app.test_request_context():
-        employee = Employee(first_name="Alice", last_name="Cooper",
-                            username="vgv", phone_number="1",
-                            birth_date=datetime.utcnow(),
-                            registration_date=datetime.utcnow(),
-                            email="test@test.com", password="pass")
-        DB.session.add(employee)
-        DB.session.commit()
-        error = login("vgv", "unknown")
-        DB.session.delete(employee)
-        DB.session.commit()
-        DB.session.remove()
-        assert ( error == "Incorrect password.")
+def test_incorrect_password(db_session):
+    employee = Employee(first_name="Alice", last_name="Cooper",
+                        username="vgv", phone_number="1",
+                        birth_date=datetime.utcnow(),
+                        registration_date=datetime.utcnow(),
+                        email="test@test.com", password="pass")
+    db_session.add(employee)
+    db_session.commit()
+    error = login("vgv", "unknown")
+    assert (error == "Incorrect password.")
 
 
-def test_authenticated(app):
-    with app.test_request_context():
-        employee = Employee(first_name="Alice", last_name="Cooper",
-                            username="vgv", phone_number="1",
-                            birth_date=datetime.utcnow(),
-                            registration_date=datetime.utcnow(),
-                            email="test@test.com", password="pass")
-        DB.session.add(employee)
-        DB.session.commit()
-        error = login("vgv", "pass")
-        DB.session.delete(employee)
-        DB.session.commit()
-        DB.session.remove()
-        assert ( error is None)
-
+def test_authenticated(db_session):
+    employee = Employee(first_name="Alice", last_name="Cooper",
+                        username="vgv", phone_number="1",
+                        birth_date=datetime.utcnow(),
+                        registration_date=datetime.utcnow(),
+                        email="test@test.com", password="pass")
+    db_session.add(employee)
+    db_session.commit()
+    error = login("vgv", "pass")
+    db_session.delete(employee)
+    db_session.commit()
+    db_session.remove()
+    assert (error is None)
