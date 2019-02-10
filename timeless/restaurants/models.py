035e6acb37c047a24d2db94cebd6d7ece38fb171
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-10 14:32:01 -0200

    For #172: Started reservation view tests

--- timeless/restaurants/models.py
+++ timeless/restaurants/models.py
@@ -118,11 +118,7 @@ class Reservation(TimestampsMixin, DB.Model):
     @todo #27:30min Calculate reservation duration in constructor
      by subtracting start_time and end_time. Don't forget to call super
      constructor in order not to override DB.Model functionality.
-    @todo #27:30min Continue implementation of views. Index and a
-     view page should be created to list all reservations. In the
-     index page there should be also a function to delete the reservation
-     (after confirmation). In the index page it should be possible
-     to sort and filter for every column.
+
     """
 
     __tablename__ = "reservations"
---
+++ timeless/restaurants/reservations/views.py
@@ -0,0 +1,46 @@
+"""Reservations views module.
+
+"""
+
+
+from flask import (
+    Blueprint, flash, redirect, render_template, request, url_for
+)
+
+
+bp = Blueprint("reservations", __name__, url_prefix="/reservations")
+
+
+@bp.route("/")
+def list_reservations(reservations):
+    flash("List not yet implemented")
+    return render_template(
+        "restaurants/tables/list.html", reservations=reservations
+    )
+
+
+@bp.route("/create", methods=("GET", "POST"))
+def create(reservation):
+    if request.method == "POST":
+        flash("Create not yet implemented")
+
+    return render_template(
+        "restaurants/tables/create_edit.html", action="create",
+        reservation=reservation
+    )
+
+
+@bp.route("/edit/<int:id>", methods=("GET", "POST"))
+def edit(id):
+    if request.method == "POST":
+        flash("Edit not yet implemented")
+    return render_template(
+        "restaurants/tables/create_edit.html", action="edit",
+        id=id
+    )
+
+
+@bp.route("/delete", methods=["POST"])
+def delete():
+    flash("Delete not yet implemented")
+    return redirect(url_for("reservations.list_reservations"))
