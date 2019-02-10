165b8d4661d25873ebca609bd7cd15e6158f14fc
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-10 15:03:34 -0200

    For #133: Review corrections

--- tests/integration/it_locations_test.py
+++ tests/integration/it_locations_test.py
@@ -3,6 +3,32 @@ from timeless.restaurants.models import Location
 from timeless.companies.models import Company
 
 
+def create_location(db_session):
+    company = Company(
+        name="Krusty Inc.",
+        code="KI",
+        address="Springfield Lane,12"
+    )
+    location =  Location(
+        name="Krusty Burger",
+        code="KB",
+        company_id=company.id,
+        country="United States",
+        region="Middle East",
+        city="Springfield",
+        address="Jebediah Street, NN",
+        longitude=23,
+        latitude=25,
+        type="B",
+        status="open",
+        comment="Fast food restaurant from a famous animated sitcom."
+    )
+    db_session.add(company)
+    db_session.add(location)
+    db_session.commit()
+    return location
+
+
 def test_create_location(db_session):
     """Integration test for adding and selecting Location"""
     location = create_location(db_session)
@@ -12,7 +38,6 @@ def test_create_location(db_session):
 def test_delete_location(db_session):
     """Integration test for deleting Location"""
     location = create_location(db_session)
-    id = location.id
     db_session.delete(location)
     assert not db_session.query(Location).get(location.id)
 
@@ -32,32 +57,6 @@ def test_edit_location(db_session):
     location.comment="An unknown fast food restaurant."
     db_session.merge(location)
     row = db_session.query(Location).get(location.id)
-    assert not row.name == "Krusty Burger"
+    assert row and row.name == location.name
     
-
-def create_location(db_session):
-    company = Company(
-        name="Krusty Inc.",
-        code="KI",
-        address="Springfield Lane,12"
-    )
-    db_session.add(company)
-    db_session.commit()
-    location =  Location(
-        name="Krusty Burger",
-        code="KB",
-        company_id=company.id,
-        country="United States",
-        region="Middle East",
-        city="Springfield",
-        address="Jebediah Street, NN",
-        longitude=23,
-        latitude=25,
-        type="B",
-        status="open",
-        comment="Fast food restaurant from a famous animated sitcom."
-    )
-    db_session.add(location)
-    db_session.commit()
-    return location
 
