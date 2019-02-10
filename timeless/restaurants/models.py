15dc09fe6b106522e861b6a92ee8937e2ce9d732
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-10 12:42:57 -0200

    For #171: Reservation duration calculation
    - Moved Reservation tests from test_models.py to test_reservation.py
    - Added Reservation duration() method and removed duration field

--- timeless/restaurants/models.py
+++ timeless/restaurants/models.py
@@ -130,13 +130,15 @@ class Reservation(TimestampsMixin, DB.Model):
     id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
     start_time = DB.Column(DB.DateTime, nullable=False)
     end_time = DB.Column(DB.DateTime, nullable=False)
-    duration = DB.Column(DB.Time, nullable=False)
     customer_id = DB.Column(DB.Integer, DB.ForeignKey("customers.id"))
     num_of_persons = DB.Column(DB.DateTime, nullable=False)
     comment = DB.Column(DB.String, nullable=False)
     status = DB.Column(DB.Enum(ReservationStatus), nullable=False)
 
     tables = DB.relationship("TableReservation", back_populates="reservation")
+
+    def duration(self):
+        return self.end_time - self.start_time
 
     def __repr__(self):
         return "<Reservation %r>" % self.id
