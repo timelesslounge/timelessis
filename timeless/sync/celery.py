3237089cc873e5b98e0b6d1538c54b7d88539418
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-12 17:13:42 -0200

    For #225: Single quotes removal

--- timeless/sync/celery.py
+++ timeless/sync/celery.py
@@ -4,8 +4,8 @@ from celery import Celery
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL']
+        backend=app.config["CELERY_RESULT_BACKEND"],
+        broker=app.config["CELERY_BROKER_URL"]
     )
     celery.conf.update(app.config)
 
