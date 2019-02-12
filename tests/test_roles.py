e248e88b370a789bea53eb7cda8d43faf36d80b8
Vedran Grgo Vatavuk <vedran.grgo.vatavuk@ericsson.com>
2019-02-12 21:10:37 +0100

    #192 roles view -> list/create

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -1,12 +1,20 @@
 from http import HTTPStatus
 
+from flask import url_for
+
+from timeless.roles.models import Role
+
 
 def test_list(client):
     assert client.get("/roles/").status_code == HTTPStatus.OK
 
 
 def test_create(client):
-    assert client.get("/roles/create").status_code == HTTPStatus.OK
+    response = client.post(url_for("role.create"), data={
+        "name": "owner"
+    })
+    assert response.location.endswith(url_for('role.list_roles'))
+    assert Role.query.count() == 1
 
 
 def test_edit(client):
