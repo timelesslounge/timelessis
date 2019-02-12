39145553b4894a5ad58446537e2fcece8ff1287c
Vedran Grgo Vatavuk <vedran.grgo.vatavuk@ericsson.com>
2019-02-12 20:13:33 +0100

    #180 owner privilages (Employee access)

--- tests/test_owner_privilages.py
+++ tests/test_owner_privilages.py
@@ -1,14 +1,47 @@
+from flask import g
+
+from datetime import datetime
+
 from timeless.access_control.methods import Method
 from timeless.access_control.owner_privileges import has_privilege
+from timeless.employees.models import Employee
 
 
 def test_can_access_location():
     assert (
-        has_privilege(method=Method.CREATE, resource="location") == True
+            has_privilege(method=Method.CREATE, resource="location") is True
     )
+
 
 def test_cant_access_unknown_resource():
     assert (
-        has_privilege(method=Method.CREATE, resource="unknown") == False
+            has_privilege(method=Method.CREATE, resource="unknown") is False
     )
 
+
+def test_cant_access_his_profile():
+    assert (
+            has_privilege(method=Method.READ, resource="employee", employee_id=1) is False
+    )
+
+
+def test_can_access_his_profile():
+    g.user = Employee(id=1, first_name="Alice", last_name="Cooper",
+                      username="alice", phone_number="1",
+                      birth_date=datetime.utcnow(),
+                      registration_date=datetime.utcnow(),
+                      email="test@test.com", password="bla")
+    assert (
+            has_privilege(method=Method.READ, resource="employee", employee_id=1) is True
+    )
+
+
+def test_can_access_own_employees():
+    g.user = Employee(id=1, first_name="Alice", last_name="Cooper",
+                      username="alice", phone_number="1",
+                      birth_date=datetime.utcnow(),
+                      registration_date=datetime.utcnow(),
+                      email="test@test.com", password="bla")
+    assert (
+            has_privilege(method=Method.READ, resource="employee") is True
+    )
--- timeless/access_control/owner_privileges.py
+++ timeless/access_control/owner_privileges.py
@@ -1,18 +1,18 @@
+from flask import g
+
 from timeless.access_control.methods import Method
 
 
 def has_privilege(method=None, resource=None, *args, **kwargs) -> bool:
     """Check if user with Owner role can access a particular resource.
-    @todo #22:30min Implement the rest of the owner privileges.
-     1) Create/modify/activate/deactivate accounts for all Employee
+    @todo #180:30min Implement the rest of the owner privileges.
+     1) Create /modify /activate /deactivate accounts for all Employee
      associated with owned Company
-     2) View self-profile
-     3) View Employee profiles associated with owned Company
     """
-    return __resources.get(resource, lambda *arg: False)(method, args)
+    return __resources.get(resource, lambda *arg: False)(method, *args, **kwargs)
 
 
-def __location_access(method=None, *args):
+def __location_access(method=None, *args, **kwargs):
     """
     @todo #22:30min Implement __location_access. Owner should
      Create / modify / delete Locations associated with owned Company.
@@ -25,6 +25,19 @@ def __location_access(method=None, *args):
     return permitted
 
 
+def __employee_access(method=None, *args, **kwargs):
+    permitted = False
+    user = g.pop("user", None)
+    employee_id = kwargs.get("employee_id")
+    if method == Method.READ and user:
+        if not employee_id:
+            permitted = True
+        else:
+            permitted = method == Method.READ and employee_id == user.id
+    return permitted
+
+
 __resources = {
-    "location": __location_access
+    "location": __location_access,
+    "employee": __employee_access
 }
