1db630d523c1049f0a7f68323011635aad032e91
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-02-12 10:52:37 +0300

    #102: fix test_data for customers

--- tests/test_poster.py
+++ tests/test_poster.py
@@ -7,14 +7,20 @@ class TestPoster:
     @classmethod
     def setup_class(cls):
         cls.port = free_port()
-        start_server(cls.port, locations={"data": "testData"}, tables={"data": "testTables"})
+        start_server(
+            cls.port,
+            locations={"data": "test_data"},
+            tables={"data": "test_data"},
+            customers={"data": "test_data"}
+
+        )
         cls.poster = Poster(url=f"http://localhost:{cls.port}")
 
     def test_locations(self):
-        assert (self.poster.locations()["data"] == "testData")
+        assert (self.poster.locations()["data"] == "test_data")
 
     def test_tables(self):
-        assert (self.poster.tables()["data"] == "testTables")
+        assert (self.poster.tables()["data"] == "test_data")
 
     def test_customers(self):
-        assert (self.poster.customers()["data"] == "testCustomers")
+        assert (self.poster.customers()["data"] == "test_data")
