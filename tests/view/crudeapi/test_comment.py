273a0a301259da2185288e13bfa62a209144f346
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-12 15:39:43 -0200

    For #222: CommentView methods tests.

--- tests/view/crudeapi/test_comment.py
+++ tests/view/crudeapi/test_comment.py
@@ -14,13 +14,14 @@ from timeless.reservations.views import Comment
 """
 
 
+@pytest.mark.skip
 def test_get_found_comment():
     commentview = CommentView()
     expected = Comment(
         id=1,
         employee_id=2,
         body="Comments for GET method",
-        date=datetime.utcnow
+        date=datetime.datetime
     )
     result = commentview.get(commentview, comment_id=5)
     assert result[0] == expected, "Wrong comment returned from CommentView"
@@ -35,73 +36,69 @@ def test_get_not_found_comment():
 
 
 @pytest.mark.skip
-def test_post_found(self):
+def test_post(self):
     commentview = CommentView()
     expected = Comment(
         id=1,
         employee_id=2,
         body="Comments for POST method",
-        date=datetime.utcnow
+        date=datetime.datetime
     )
-    result = commentview.post(commentview, comment_id=5)
+    result = commentview.post(commentview)
     assert result[0] == expected, "Wrong comment returned from CommentView"
-    assert result[1] == HTTPStatus.OK, "Wrong status returned from CommentView"
+    assert result[1] == HTTPStatus.CREATED, "Wrong status returned from CommentView"
 
 
-def test_post_not_found_comment():
-    commentview = CommentView()
-
-    with pytest.raises(werkzeug.exceptions.NotFound):
-        result = commentview.post(commentview, comment_id=5)
-
-
-
+@pytest.mark.skip
 def test_delete_found():
     commentview = CommentView()
     expected = Comment(
         id=1,
         employee_id=2,
         body="Comments for DELETE method",
-        date=datetime.utcnow
+        date=datetime.datetime
     )
     result = commentview.delete(commentview, comment_id=5)
     assert result[1] == HTTPStatus.NO_CONTENT, "Wrong status returned from CommentView"
 
 
+@pytest.mark.skip
 def test_delete_not_found():
     commentview = CommentView()
-    result = commentview.delete(commentview, comment_id=5)
+
     with pytest.raises(werkzeug.exceptions.NotFound):
-        result = commentview.post(commentview, comment_id=5)
+        result = commentview.delete(commentview, comment_id=1)
 
 
+@pytest.mark.skip
 def test_put_comment():
     commentview = CommentView()
     expected = Comment(
         id=1,
         employee_id=2,
         body="Comments for PUT method",
-        date=datetime.utcnow
+        date=datetime.datetime
     )
     result = commentview.put(commentview)
     assert result[0] == expected, "Wrong comment returned from CommentView"
     assert result[1] == HTTPStatus.OK, "Wrong status returned from CommentView"
 
 
+@pytest.mark.skip
 def test_put_found_comment():
     commentview = CommentView()
     original = Comment(
         id=1,
         employee_id=2,
         body="Comments for PUT method that does not exists",
-        date=datetime.utcnow
+        date=datetime.datetime
     )
     commentview.put(commentview)
     altered = Comment(
         id=1,
         employee_id=2,
         body="Comments for PUT method that does not exists",
-        date=datetime.utcnow
+        date=datetime.datetime
     )
     result = commentview.put(commentview)
     assert result[0] == altered, "Wrong comment returned from CommentView"
