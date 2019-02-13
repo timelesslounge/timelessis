d1ac77f070e153795cf6d119c35ac5b728553c49
Eugene Kondrashev <eugene.kondrashev@gmail.com>
2019-02-13 18:02:09 +0200

    For 240: Removed TODO so it is not treated as puzzle

--- scripts/install/deploy/install_redis.sh
+++ scripts/install/deploy/install_redis.sh
@@ -5,4 +5,4 @@
 #  In case it is installed but not among current process it should be launched only.
 #  Please also check if all the dependency checks are in place besides redis and implement rest if any.
 
-echo "TODO: Implement redis availability check"
+echo "Implement redis availability check"
