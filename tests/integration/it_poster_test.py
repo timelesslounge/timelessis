18f0b81034290a19ecef151162a19e69b7fae717
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-02-13 22:38:02 +0300

    197: fix tests

--- tests/integration/it_poster_test.py
+++ tests/integration/it_poster_test.py
@@ -5,20 +5,26 @@ from timeless.poster.api import Authenticated, PosterAuthData
 """Integration tests for Poster"""
 
 
-def test_auth():
+@mock.patch("timeless.poster.api.requests")
+def test_auth(requests_mock):
     auth_data = PosterAuthData(
         application_id="test_id",
         application_secret="test_secret",
         redirect_uri="test_uri",
         code="test_code",
     )
-    auth_token = "test_auth_token"
+    auth_token = "861052:02391570ff9af128e93c5a771055ba88"
+    json_object = mock.Mock()
+    json_object.return_value = {"access_token": auth_token}
 
-    def auth(*args, **kwargs):
-        mocked = mock.Mock()
-        mocked.return_value = auth_token
-        return mocked
+    requests_mock.post.return_value = type(
+        'Mock date',
+        (),
+        {
+            'ok': True,
+            'json': json_object,
+        }
+    )
 
-    with mock.patch.object(Authenticated, 'auth', auth):
-        auth_token = Authenticated(auth_data).auth()
-        assert auth_token == auth_token
+    auth_token = Authenticated(auth_data).auth()
+    assert auth_token == auth_token
