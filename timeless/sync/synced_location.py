f2f0f08c0524106b2647c0650ac237562d078182
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-14 18:48:13 -0200

    For #225: Correcting puzzle.

--- timeless/sync/synced_location.py
+++ timeless/sync/synced_location.py
@@ -7,7 +7,7 @@ from timeless.sync.sync import PosterSync
     @todo #157:30min Implement synchronization between Poster and Database for
      Locations. Data coming from Poster has priority upon data stored in our
      database. 
-     
+
     @todo #225:30min Use celery instead jobs cron-based jobs. Celery is already 
      being used to sync customers with postes; let's configure it and make a 
      job to run locations sync too.  
