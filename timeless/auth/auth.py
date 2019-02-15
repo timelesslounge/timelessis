315a4ebd73d4ae8de7199d9da1b4258a9b7141e4
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-15 09:07:30 -0200

    For #221: Lint corrections.

--- timeless/auth/auth.py
+++ timeless/auth/auth.py
@@ -4,10 +4,11 @@ from timeless.employees.models import Employee
 
 def login(username="", password=""):
     """Login user
-    @todo #59:30min The backend should return only error codes for login procedure,
-     not hardcoded messages. The codes should then be translated into (mapped to)
-     Strings on the UI, so we can easily have internationalization or change
-     error messages without having to rebuild + redeploy the backend.
+    @todo #59:30min The backend should return only error codes for login
+     procedure, not hardcoded messages. The codes should then be translated
+     into (mapped to) Strings on the UI, so we can easily have
+     internationalization or change error messages without having to
+     rebuild + redeploy the backend.
     """
     user = Employee.query.filter_by(username=username).first()
     error = None
@@ -19,4 +20,3 @@ def login(username="", password=""):
         session.clear()
         session["user_id"] = user.id
     return error
-
