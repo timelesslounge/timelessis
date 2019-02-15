e183bb6d0371bf585bf5ef90e3a724b73b2dad81
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-15 08:43:08 -0200

    For #221: Lint corrections.

--- tests/view/crudapi/test_crud_api.py
+++ tests/view/crudapi/test_crud_api.py
@@ -1,35 +1,30 @@
-import pytest
-
-
+""" Tests for CrudeAPIView. """
 from http import HTTPStatus
-
-from timeless.views import FakeAPIView
-from timeless.reservations.views import CommentView
+import pytest
 from werkzeug.exceptions import NotFound
-"""
-    Tests for CrudeAPIView.
-    
-"""
+from timeless.views import FakeAPIView
+
 
 @pytest.mark.skip
 def test_get_found_object(app):
+    """ Tests for CrudeAPIView get method when the object exists. """
     with app.test_request_context(
-        '/test/crudapitest',
-        data = {'some_id':5}
+            "/test/crudapitest",
+            data={"some_id":5}
     ):
         apiview = FakeAPIView()
         result = apiview.get()
-    assert result[0] == { "some_id" : 5}, "Wrong result returned from CrudeAPI view"
+    assert result[0] == {"some_id" : 5}, "Wrong result returned from CrudeAPI view"
     assert result[1] == HTTPStatus.OK, "Wrong response from CrudeAPI view"
 
 
 @pytest.mark.skip
 def test_get_not_found_object(app):
+    """ Tests for CrudeAPIView get method when the object does not exists. """
     with app.test_request_context(
-            '/api/crudapi',
-            data = {'some_id':5}
+            "/api/crudapi",
+            data={"some_id":5}
     ):
         apiview = FakeAPIView()
         with pytest.raises(NotFound, message="Fake object not found"):
             apiview.get()
-
--- timeless/views.py
+++ timeless/views.py
@@ -1,62 +1,69 @@
-from http import HTTPStatus
-from flask import request, views
-from werkzeug.exceptions import abort
+"""View that supports generic crud operations.
+Example of using CrudAPIView:
 
-from timeless.db import DB
+class CommentView(CrudView):
+
+     model = Comment
+     url_lookup = "comment_id"
+
+@todo #221:30min Remove references to database objects. we should not tie
+ our view layer to our database objects. We should create some form of
+ abstraction to handle the model calls, for example,
+ model.query.get(object_id) that would decorate the real implementation,
+ eliminating the coupling here.
+"""
+from http import HTTPStatus
+from flask import views
+from werkzeug.exceptions import abort
 
 
 class CrudAPIView(views.MethodView):
     """View that supports generic crud operations.
-    Example of using CrudAPIView:
 
-    class CommentView(CrudView):
-
-         model = Comment
-         url_lookup = "comment_id"
-
-    @todo #221:30min Remove references to database objects. we should not tie
-     our view layer to our database objects. We should create some form of
-     abstraction to handle the model calls, for example,
-     model.query.get(object_id) that would decorate the real implementation,
-     eliminating the coupling here.
-    """
-    model = None
-    url_lookup = None
-
-    """
     @todo #221:30min Continue with the implementation of CrudAPIView.
-     Implement get, post, put and delete methods. We should return json representation of object model in methods. Use 
-     FakeModel for a fake database object, and implement the desired calls on FakeQuery to get, create, save / update 
-     and delete returnin the result. Don't forget to implement the tests too, to test if CrudAPIView code is being 
-     called and returning the expected objects; please refer to #221 and #222 for documentation. After that remove the 
-     ignore annotation from tests on test_crud_api.py.
+     Implement get, post, put and delete methods. We should return json
+     representation of object model in methods. Use FakeModel for a fake
+     database object, and implement the desired calls on FakeQuery to get,
+     create, save / update and delete returning the result. Don't forget to
+     implement the tests too, to test if CrudAPIView code is being called and
+     returning the expected objects; please refer to #221 and #222 for
+     documentation. After that remove the ignore annotation from tests on
+     test_crud_api.py.
     """
 
     def get(self):
+        """Calls the GET method."""
         pass
 
     def post(self):
+        """Calls the POST method."""
         pass
 
     def put(self):
+        """Calls the PUT method."""
         pass
 
     def delete(self):
+        """Calls the DELETE method."""
         pass
 
 
 class FakeModel():
+    """Fake model for tests."""
 
-    class FakeQuery():
+    class FakeQuery:
+        """Fake query for tests."""
 
-        def get(object_id):
+        def get(self, object_id):
+            """Fake response on get method."""
             if object_id == 5:
-                return {"Found the object"},HTTPStatus.OK
+                return {"Found the object"}, HTTPStatus.OK
             abort(HTTPStatus.NOT_FOUND)
 
     query = FakeQuery()
 
 
 class FakeAPIView(CrudAPIView):
+    """Fake CrudAPI imoplementation for tests."""
     model = FakeModel
     url_lookup = "some_id"
