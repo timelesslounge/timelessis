372235e21db2cfcf6502debe771a68d6e677235f
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-02-18 09:37:27 +0100

    #174 remove old puzzle

--- timeless/access_control/administrator_privileges.py
+++ timeless/access_control/administrator_privileges.py
@@ -7,12 +7,6 @@ def has_privilege(method=None, resource=None, *args, **kwargs) -> bool:
 
 
 def __location_access(method=None, *args, **kwargs):
-    """
-    @todo #22:30min Implement __location_access. Owner should
-     Create / modify / delete Locations associated with owned Company.
-     Fetch owner from g.user (if it doesnt exist than fetch its id from
-     session. Location id can be obtained from args.
-    """
     return True
 
 
