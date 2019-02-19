b7300ebbdc68537533131470d4652acb543cf1c2
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-02-20 00:22:37 +0300

    #187: leave puzzle

--- timeless/poster/tasks.py
+++ timeless/poster/tasks.py
@@ -2,17 +2,15 @@ import os
 
 from celery import shared_task
 
+from timeless import DB
 from timeless.poster.api import Authenticated, PosterAuthData, Poster
-from timeless.restaurants import models
+from timeless.restaurants.models import Table
 
 
 @shared_task
 def sync_tables():
     """
     Periodic task for fetching and saving tables from Poster
-    @todo #187:30min Implement integration tests for this periodic task.
-     Create fixture and mock response from poster, and check how it will be
-     saved in DB. API docs - https://dev.joinposter.com/en/docs/api
     @todo #187:30min Set up scheduler for celery,
      docs - http://docs.celeryproject.org/en/
      latest/userguide/periodic-tasks.html#id5
@@ -28,14 +26,14 @@ def sync_tables():
     auth_token = Authenticated(auth_data=auth_data).auth()
     poster = Poster(auth_token=auth_token)
     for poster_table in poster.tables():
-        table = models.Table.query.filter_by(
+        table = DB.session(Table).query.filter_by(
             name=poster_table["name"], floor_id=poster_table["floor_id"]
         ).first()
 
         if table:
-            for key, value in table.items():
-                setattr(table, key, value)
+            new_table = Table.merge_with_poster(table, poster_table)
         else:
-            table = models.Table(**poster_table)
+            new_table = Table.create_by_poster(poster_table)
 
-        table.save()
+        DB.session.add(new_table)
+        DB.session.flush()
