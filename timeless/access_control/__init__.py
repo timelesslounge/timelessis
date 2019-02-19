e4685e40d40526ef284deb880f2cd89f55818eb3
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-02-19 10:45:40 +0100

    #176 remved puzzle

--- timeless/access_control/__init__.py
+++ timeless/access_control/__init__.py
@@ -1,7 +1,4 @@
 """
-@todo #22:30min Implement Manager access control by implementing privileges
- logic and including it to the authorization functionality.
- See Issue #22 for more details about his privileges.
 @todo #22:30min Implement Location Admin access control by
  implementing privileges logic and including it to the
  authorization functionality. See Issue #22 for more details about
