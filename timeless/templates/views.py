ae3cefea0ba9f338b94b69bacc5e2b2707c9f149
Eugene Kondrashev <eugene.kondrashev@gmail.com>
2019-02-19 12:02:13 +0200

    #260: Reduced line length

--- timeless/templates/views.py
+++ timeless/templates/views.py
@@ -4,4 +4,4 @@ def order_by(query, params):
 
 
 def filter_by(query, params):
-    return query.filter_by(**(dict(param.split("=", maxsplit=1) for param in params)))
+    return query.filter_by(**(dict(p.split("=", maxsplit=1) for p in params)))
