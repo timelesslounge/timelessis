dadbef0d55d75b32e916f653d98da410f6ad927a
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-02-19 16:46:49 +0100

    #181 fixing todo

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,7 +4,8 @@ from timeless.access_control.authorization import is_allowed
 from timeless.access_control.methods import Method
 
 
-# @todo #181:30min This test stopped working because it wasn't correct.
+# @todo #181:30min This test stopped working because it was not working
+#  according to requirements.
 #  Write new tests that validate 'is_allowed' according to specifications in #22
 @pytest.mark.skip("fix me")
 def test_owner_can_access_location():
