c7e6a66f8531549150b408ddcd94a181eb54d980
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2019-02-20 23:13:29 +0300

    #178 Master / Other role permissions

--- tests/integration/it_other_privilages_test.py
+++ tests/integration/it_other_privilages_test.py
@@ -6,7 +6,7 @@ from timeless.access_control.other_privileges import has_privilege
 from timeless.access_control.methods import Method
 
 
-@pytest.mark.parametrize('method', (
+@pytest.mark.parametrize("method", (
     Method.READ,
     Method.CREATE,
     Method.UPDATE,
@@ -15,7 +15,8 @@ from timeless.access_control.methods import Method
 def test_can_access_to_self_account(method, app):
     employee = factories.EmployeeFactory()
     flask.g.user = employee
-    assert has_privilege(method=method, resource="employee", employee_id=employee.id)
+    assert has_privilege(
+        method=method, resource="employee", employee_id=employee.id)
 
 
 @pytest.mark.parametrize('method', (
@@ -27,4 +28,5 @@ def test_can_access_to_self_account(method, app):
 def test_cannot_access_to_foreign_account(method, app):
     employee = factories.EmployeeFactory()
     flask.g.user = employee
-    assert not has_privilege(method=method, resource="employee", employee_id=-1)
+    assert not has_privilege(
+        method=method, resource="employee", employee_id=-1)
