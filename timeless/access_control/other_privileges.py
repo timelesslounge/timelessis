316ede83f10f8baeb8d5aa94ed5498005492e3c8
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2019-02-20 23:19:19 +0300

    #178 Master / Other role permissions

--- timeless/access_control/other_privileges.py
+++ timeless/access_control/other_privileges.py
@@ -2,20 +2,18 @@
 import flask
 
 
-def has_privilege(method=None, resource=None, *args, **kwargs) -> bool:
+def has_privilege(resource=None, *args, **kwargs) -> bool:
     """Check if user with Master / Intern / Others role can access a
     particular resource."""
-    return __resources.get(resource, lambda *arg: False)(
-        method, *args, **kwargs
-    )
+    return __resources.get(resource, lambda *arg: False)(*args, **kwargs)
 
 
-def __employee_access(method=None, *args, **kwargs):
+def __employee_access(*args, **kwargs):
     """Owner of this role can view and update only self"""
     user = flask.g.get("user")
-    return kwargs.get('employee_id') == user.id if user else False
+    return kwargs.get("employee_id") == user.id if user else False
 
 
 __resources = {
-    'employee': __employee_access
+    "employee": __employee_access
 }
