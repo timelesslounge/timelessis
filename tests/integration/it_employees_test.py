036be71d6001eda7ac25ba3742ed84b4e4f64062
Boris Kuzmic <boris.kuzmic@gmail.com>
2019-02-20 21:36:37 +0100

    #57 Fixing PR issues

--- tests/integration/it_employees_test.py
+++ tests/integration/it_employees_test.py
@@ -1,19 +1,17 @@
+"""Integration tests for Employees"""
 from http import HTTPStatus
 from datetime import datetime
+
 from timeless.employees.models import Employee
 
 
 def test_insert_employee(db_session):
     """Integration test for adding and selecting Employee"""
-    employee = Employee(first_name="Alice", last_name="Cooper",
-                        username="alice", phone_number="1", account_status="1",
-                        birth_date=datetime.utcnow(), pin_code=1234,
-                        registration_date=datetime.utcnow(), user_status="1",
-                        email="test@test.com", password="bla")
+    employee = create_employee()
     db_session.add(employee)
     db_session.commit()
     row = db_session.query(Employee).get(employee.id)
-    assert row.username == "alice"
+    assert row.username == "john"
 
 
 def test_timestamp_mixin_created_on(db_session):
@@ -49,6 +47,7 @@ def test_timestamp_mixin_updated_on(db_session):
 
 
 def create_employee():
+    """ Create new instance of Employee to reuse in other tests """
     return Employee(
         first_name="John", last_name="Smith",
         username="john", phone_number="123", birth_date=datetime.utcnow(),
@@ -60,13 +59,9 @@ def create_employee():
 
 def test_list(client, db_session):
     """ List all employees """
-    employee = Employee(first_name="Alice", last_name="Cooper",
-                        username="alice", phone_number="1", account_status="1",
-                        birth_date=datetime.utcnow(), pin_code=1234,
-                        registration_date=datetime.utcnow(), user_status="1",
-                        email="test@test.com", password="bla")
+    employee = create_employee()
     db_session.add(employee)
     db_session.commit()
-    employees = client.get("/employees/")
-    assert employees.status_code == HTTPStatus.OK
-    assert b"Alice Cooper" in employees.data
+    response = client.get("/employees/")
+    assert response.status_code == HTTPStatus.OK
+    assert b"John Smith" in response.data
