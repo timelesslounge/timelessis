85978f53e134b2e5569e29508ea1b8ada24f9de7
Vadim Ryazanov <vadimvoronezh@yandex.ru>
2019-02-21 13:11:20 +0300

    #178 Review corrections

--- tests/integration/it_other_privilages_test.py
+++ tests/integration/it_other_privilages_test.py
@@ -12,21 +12,11 @@ from timeless.access_control.methods import Method
     Method.UPDATE,
     Method.DELETE
 ))
-def test_can_access_to_self_account(method, app):
+def test_can_access_to_emloyee_resource(method, app):
+    """ Check that user can access only to own employee account """
     employee = factories.EmployeeFactory()
     flask.g.user = employee
     assert has_privilege(
         method=method, resource="employee", employee_id=employee.id)
-
-
-@pytest.mark.parametrize('method', (
-    Method.READ,
-    Method.CREATE,
-    Method.UPDATE,
-    Method.DELETE
-))
-def test_cannot_access_to_foreign_account(method, app):
-    employee = factories.EmployeeFactory()
-    flask.g.user = employee
     assert not has_privilege(
         method=method, resource="employee", employee_id=-1)
