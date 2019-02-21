3adb40356397d2fedcf3dc1c3c043632ff76fd04
Boris Kuzmic <boris.kuzmic@ericsson.com>
2019-02-21 13:46:31 +0100

    #208 Fixing PR issues

--- tests/integration/it_locations_test.py
+++ tests/integration/it_locations_test.py
@@ -1,37 +1,35 @@
 """Integration tests for Locations"""
 from http import HTTPStatus
 from flask import url_for
-import pytest
 
 from timeless.restaurants.models import Location
 
 
 def test_list(client, db_session):
     """ List all locations """
-    db_session.add(__create_location__())
+    db_session.add(_create_test_location())
     db_session.commit()
     response = client.get(url_for("location.list"))
     assert response.status_code == HTTPStatus.OK
     assert b"Test location" in response.data
+    assert response.data.count(b"<article class=\"location\">",
+                               response.data.find(b"<!doctype html>")) == 1
 
 
-@pytest.mark.skip(reason="location.create() is not yet implemented")
-def test_create(client, db_session):
+def test_create(client):
     assert client.get("/locations/create").status_code == HTTPStatus.OK
 
 
-@pytest.mark.skip(reason="location.edit(id) is not yet implemented")
 def test_edit(client):
     assert client.get("/locations/edit/1").status_code == HTTPStatus.OK
 
 
-@pytest.mark.skip(reason="location.delete() is not yet implemented")
-def test_delete(client, db_session):
+def test_delete(client):
     response = client.post("/locations/delete", data={"id": 1})
     assert response.headers["Location"] == "http://localhost/locations/"
 
 
-def __create_location__():
+def _create_test_location():
     return Location(
         name="Test location", code="1", country="USA", region="West",
         city="LA", type="Type", address="Address", longitude="11223344",
--- timeless/restaurants/locations/views.py
+++ timeless/restaurants/locations/views.py
@@ -1,14 +1,16 @@
 """Locations views module.
 @todo #208:30min Continue implementing Create, Edit and Delete views once
- generic views from #173 are implemented. Templates are already provided below,
- so uncomment and modify - create view templates and write ITs to verify
- behaviour.
+ generic views from #173 are implemented. Code templates are already provided
+ below, so uncomment and modify - create view templates and write ITs to verify
+ behaviour. Remove function endpoints once the view classes are in place.
 @todo #208:30min Continue implementing List to enable sorting and filtering
  for every column. For this, probably there will be a need to create a new
  generic view that all other List views will extend. This generic view should
  use GenericFilter implemented in #317.
 """
-from flask import Blueprint
+from flask import (
+    Blueprint, flash, redirect, render_template, request, url_for
+)
 
 from timeless.views import ListView
 from timeless.restaurants.models import Location
@@ -21,6 +23,13 @@ bp = Blueprint("location", __name__, url_prefix="/locations")
 #     template_name = "restaurants/locations/create_edit.html"
 #     form_class  = LocationForm
 #     model = Location
+@bp.route("/create", methods=("GET", "POST"))
+def create():
+    if request.method == "POST":
+        flash("Create not yet implemented")
+    action = "create"
+    return render_template("restaurants/locations/create_edit.html",
+                           action=action)
 
 
 # class Edit(UpdateView):
@@ -28,6 +37,13 @@ bp = Blueprint("location", __name__, url_prefix="/locations")
 #     template_name = "restaurants/locations/create_edit.html"
 #     form_class  = LocationForm
 #     model = Location
+@bp.route("/edit/<int:id>", methods=("GET", "POST"))
+def edit(id):
+    if request.method == "POST":
+        flash("Edit not yet implemented")
+    action = "edit"
+    return render_template("restaurants/locations/create_edit.html",
+                           action=action)
 
 
 # class Delete(DeleteView):
@@ -36,6 +52,10 @@ bp = Blueprint("location", __name__, url_prefix="/locations")
 #     """
 #     form_class  = LocationForm
 #     model = Location
+@bp.route("/delete", methods=["POST"])
+def delete():
+    flash("Delete not yet implemented")
+    return redirect(url_for("location.list"))
 
 
 class List(ListView):
