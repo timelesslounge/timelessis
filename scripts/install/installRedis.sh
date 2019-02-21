e84edad4eabe53a8742c6a4921036326be2db30f
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-21 14:26:32 -0300

    For #300: Reverting changes

--- scripts/install/installRedis.sh
+++ scripts/install/installRedis.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-sudo apt -y install make gcc libc6-dev tcl
+sudo apt install make gcc libc6-dev tcl
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 sudo make install
-ls /var/log/redis
-if [ "$?" -gt "0" ]; then
-    sudo mkdir /var/log/redis
-fi
-sudo chown redis /var/log/redis
-sudo chgrp redis /var/log/redis
-sudo chmod +w /var/log/redis
-sudo src/redis-server > /var/log/redis/redis.log &
+src/redis-server > /dev/null &
