6ff0a3a2463f20c681f48b3ecc9c9b34106f45d0
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-21 13:52:22 -0300

    For #300: Permission error

--- scripts/install/installRedis.sh
+++ scripts/install/installRedis.sh
@@ -4,4 +4,8 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 sudo make install
+sudo /var/log/redis
+sudo chown redis /var/log/redis
+sudo chgrp redis /var/log/redis
+sudo chmod +w /var/log/redis
 sudo src/redis-server > /var/log/redis &
