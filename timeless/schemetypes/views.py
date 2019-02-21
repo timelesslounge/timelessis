1f424d0a473226f0bb545b5ed6dee13f4d082399
Boris Kuzmic <boris.kuzmic@ericsson.com>
2019-02-21 14:15:08 +0100

    #56 Adding new puzzle to continue List view implementation

--- timeless/schemetypes/views.py
+++ timeless/schemetypes/views.py
@@ -3,10 +3,9 @@
  generic views from #173 are implemented. Code templates are already provided
  below, so uncomment and modify - create templates and write ITs to verify
  behaviour.
-@todo #56:30min Continue implementing List to enable sorting and filtering
- for every column. For this, probably there will be a need to create a new
- generic view that all other List views will extend. This generic view should
- use GenericFilter implemented in #317.
+@todo #56:30min Once #357 is finished, reuse the view (or better create a new
+ generic one - if it doesn't exist) to enable sorting and filtering for every
+ column. Also replace all other List views with this new generic one.
 """
 from flask import Blueprint
 
