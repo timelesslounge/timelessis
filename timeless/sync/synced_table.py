9ea2822f384d136e092005b6b4037d702174b8c6
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-22 10:11:43 -0300

    For #342: Code climate corrections

--- timeless/sync/synced_table.py
+++ timeless/sync/synced_table.py
@@ -5,7 +5,7 @@ from timeless.sync.sync import PosterSync
     Table synced to database.
     @todo #342:30min Implement synchronization between Poster and Database for
      Tables. Data coming from Poster has priority upon data stored in our
-     database. Synchronization must be done via celery job. See sync of 
+     database. Synchronization must be done via celery job. See sync of
      Location implementation as reference. Tests for poster sync are already
      created in it_sync_tables_test.py
 """
