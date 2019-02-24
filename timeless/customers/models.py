0355e98f288d06d09ef6160d88d0771220bf99e3
Vatavuk <vatavuk1@hotmail.com>
2019-02-24 08:20:43 +0100

    #262 Customer Sync with Poster

--- timeless/customers/models.py
+++ timeless/customers/models.py
@@ -24,3 +24,27 @@ class Customer(PosterSyncMixin, DB.Model):
 
     def __repr__(self):
         return "<Customer(name=%s %s)>" % (self.first_name, self.last_name)
+
+    @classmethod
+    def merge_with_poster(cls, customer, poster_customer: dict):
+        """
+        Method should return Customer object with merged data from table entity
+        and poster customer dict
+        @todo #262:30min Implement two class methods merge_with_poster and
+         create_by_poster. merge_with_poster will merge entry entity with
+         poster entity, we should make right fields mapping, as result
+         returns Customer instance.
+         The same should be made with method create_by_poster, returns Customer
+         instance with data from poster_customer
+        """
+        return cls()
+
+    @classmethod
+    def create_by_poster(cls, poster_customer: dict):
+        """
+        Method should return Table object with given data from
+        poster_table dict
+        """
+        poster_customer
+        return cls()
+
--- timeless/poster/tasks.py
+++ timeless/poster/tasks.py
@@ -4,8 +4,24 @@ import os
 from celery import shared_task
 
 from timeless import DB
+from timeless.customers.models import Customer
 from timeless.poster.api import Authenticated, PosterAuthData, Poster
 from timeless.restaurants.models import Table
+
+
+def __poster_api():
+    auth_data = PosterAuthData(
+        application_id=os.environ.get("poster_application_id"),
+        application_secret=os.environ.get("poster_application_secret"),
+        redirect_uri=os.environ.get("poster_redirect_uri"),
+        code=os.environ.get("poster_code"),
+    )
+    auth_token = Authenticated(auth_data=auth_data).auth()
+    poster = Poster(auth_token=auth_token)
+    return poster
+
+
+__poster = __poster_api()
 
 
 @shared_task
@@ -18,24 +34,28 @@ def sync_tables():
      Also should make small refactoring: celery.py should situated in
      timelessis/celery.py not in timelessis/sync/celery.py
     """
-    auth_data = PosterAuthData(
-        application_id=os.environ.get("poster_application_id"),
-        application_secret=os.environ.get("poster_application_secret"),
-        redirect_uri=os.environ.get("poster_redirect_uri"),
-        code=os.environ.get("poster_code"),
-    )
-    auth_token = Authenticated(auth_data=auth_data).auth()
-    poster = Poster(auth_token=auth_token)
-    for poster_table in poster.tables():
+    for poster_table in __poster.tables():
         table = DB.session(Table).query.filter_by(
             name=poster_table["name"], floor_id=poster_table["floor_id"]
         ).first()
+        merge_data(model=Table, poster_data=poster_table, timelessis_data=table)
 
-        if table:
-            new_table = Table.merge_with_poster(table, poster_table)
-            DB.session.add(new_table)
-        else:
-            new_table = Table.create_by_poster(poster_table)
-            DB.session.merge(new_table)
 
-        DB.session.commit()
+@shared_task
+def sync_customers():
+    """
+    Periodic task for fetching and saving tables from Customer
+    """
+    for poster_customer in __poster.customers():
+        customer = DB.session(Customer).query.filter_by(
+            first_name=poster_customer["first_name"], last_name=poster_customer["last_name"]
+        ).first()
+        merge_data(model=Customer, poster_data=poster_customer, timelessis_data=customer)
+
+
+def merge_data(model, poster_data, timelessis_data):
+    if timelessis_data:
+        DB.session.add(model.merge_with_poster(timelessis_data, poster_data))
+    else:
+        DB.session.merge(model.create_by_poster(poster_data))
+    DB.session.commit()
