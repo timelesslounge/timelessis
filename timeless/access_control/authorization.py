de584dace95141e253664e92f73e0f5004ec0442
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-26 00:06:55 -0300

    For #182: Removing todo

--- timeless/access_control/authorization.py
+++ timeless/access_control/authorization.py
@@ -11,9 +11,6 @@ def is_allowed(method=None, resource=None, *args, **kwargs) -> bool:
     args or kwargs. This method is meant to work in conjunction with
     SecuredView.dispatch_request so that all available information about
     a user view can be accessible in the authorization process.
-    @todo #22:30min Get role from actual user instead of using
-     hardcoded value. User can be fetched from g.user. Make sure
-     that access is not allowed for unknown role.
     @todo #242:30min Implement the rest of the owner privileges.
      Owner should be able to create, modify, activate and deactivate accounts
      for employess of his/her company. Create the tests for each of this
