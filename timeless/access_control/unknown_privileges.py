96457f4a3bf841b85b128a47ca0e0900b42614a8
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-26 17:20:10 -0300

    For #182: PEP8 corrections

--- timeless/access_control/unknown_privileges.py
+++ timeless/access_control/unknown_privileges.py
@@ -1,6 +1,10 @@
-""" Permissions for Unknown roles """
+""" Permissions for Unknown roles
+@todo #182:30min Create tests for `unknown_privilege.has_privilege`. Users
+ which does not have eny privilege should fall into this category and doesn't
+ have access to any part of the system.
+"""
 
 
-def has_privilege(*args, resource=None, **kwargs) -> bool:
+def has_privilege(*args, resource=None, **kwargs):
     """Unknown role does not have any privileges."""
     return False
