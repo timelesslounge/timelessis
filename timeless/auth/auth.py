1699a05932e04ad43dff82d581820332e0df4cf1
Ammar Atef <a_atef45@yahoo.com>
2019-02-28 10:54:16 +0200

    #389 removed the puzzle, no longer needed

--- timeless/auth/auth.py
+++ timeless/auth/auth.py
@@ -1,14 +1,7 @@
 from flask import session
 from timeless.employees.models import Employee
 
-"""
-    @todo #370:30min Decouple routines from database. Database
-     implementation and routines are tightly coupled, which prevents
-     unit testing. Decouple Employee model from routines creating Employee and
-     Employees abstractions (see
-     https://github.com/timelesslounge/timelessis/pull/375) for examples.
-     Then create mocks and use these mocks to test auth.
-"""
+
 def login(username="", password=""):
     """Login user
 
