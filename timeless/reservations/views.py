e3b8d2fe3f66b8661f319be1ea11c3ffabd5d5b3
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-02-28 23:00:20 -0300

    For #293: Implementing reservation Settings template screens

--- timeless/reservations/views.py
+++ timeless/reservations/views.py
@@ -18,10 +18,6 @@ bp = Blueprint("reservations", __name__, url_prefix="/reservations")
 class SettingsList(views.ListView):
     """
     List view set for Reservation Settings
-    @todo #186:30min For SettingsListView, SettingsCreateView,
-     SettingsDetailView, SettingsDeleteView create correct templates
-     for list, create/detail actions. When templates will be done, pls change
-     `template_name` value in every View Class.
     @todo #173:30min Refactor (and uncomment) views below to use new
      base views once when they are avaiable. Current implementation is not
      generic enough.
