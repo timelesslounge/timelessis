29a59ecbf3b64b783611f6326d66d9edb0928e47
Paulo Lobo <pauloeduardolobo@gmail.com>
2019-03-04 18:10:49 -0300

    For #374: Review corrections

--- scripts/install/deploy/install_db.sh
+++ scripts/install/deploy/install_db.sh
@@ -33,6 +33,7 @@ sudo systemctl start timeless_pg.service
 if [! psql -lqt | cut -d \| -f 1 | grep -qw "timelessdb"; ]; then
     echo "Creating database: timelessdb"
     sudo -u postgres psql -c "CREATE DATABASE timelessdb;"
+    echo "Timeless database created successfully"
 fi
 
 #checks if the user exists and create if not
@@ -45,22 +46,18 @@ if [! psql -t -c '\du' | cut -d \| -f 1 | grep -qw "timeless_user"; ]; then
         INHERIT
         LOGIN
         ENCRYPTED PASSWORD 'timeless_pwd';"
+    echo "Timeless user created successfully"
 fi
 
-company = $(sudo -u postgres -H -- psql -d timelessdb -c "INSERT INTO company
-(name, code, address) values ('Timeless', 'Tm', '')")
-role = $(sudo -u postgres -H -- psql -d timelessdb -c "INSERT INTO role
-(name, works_on_shifts, company_id) values ('Administrator', False, $company)")
+company = $(sudo -u postgres -H -- psql -d timelessdb -c "INSERT INTO company (name, code, address) values ('Timeless', 'Tm', '')")
+role = $(sudo -u postgres -H -- psql -d timelessdb -c "INSERT INTO role (name, works_on_shifts, company_id) values ('Administrator', False, $company)")
 password = "pass from rultor"
 sudo -u postgres -H -- psql -d timelessdb -c "INSERT INTO employee
     (first_name, last_name, username, phone_number, birth_date,
     registration_date, account_status, user_status, email, password, pin_code,
-    comment, company_id, role_id) values
+    comment, company_id, role_id)
+    values
     ('First', 'Last', 'timeless', '988888', '', '', 'active', 'active',
     'abc@xyz.com', $password, '10', 'Timeless user', $company, $role
     )
     "
-
-
-
-
