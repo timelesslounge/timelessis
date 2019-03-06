0756dbf691c1ddd2826a91964ab8d0ac50215bb8
Eber <37782410+ebercamargo@users.noreply.github.com>
2019-03-06 09:42:34 -0300

    Update views.py

--- timeless/reservations/views.py
+++ timeless/reservations/views.py
@@ -115,11 +115,14 @@ class ReservationView(views.CrudAPIView):
 def list():
     """list """
 
+def list():
     """
     @todo #215:30min Replace this for ReservationsListView(views.ListView)
-        in all tests.Because ReservationsListView(views.ListView) covers
-        the needs of this and after using reservationListView more tests
-        break because using this implementation.
+    Take a look at puzzles #134 and #173 where the requirements of generic
+    in all tests.Because ReservationsListView(views.ListView) covers
+    views are described. Don't forget to cover the generated code with
+    the needs of this and after using reservationListView more tests
+    tests break because using this implementation.
 
     :param reservations:
     :return:
