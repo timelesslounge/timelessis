96d2cee70ab19b565d1cf6915d0deb9c26b7f02d
Emiliano Della Casa <emiliano.dellacasa@gmail.com>
2019-03-11 11:21:05 +0100

    Issue #422 - Fix for review comments

--- tests/integration/it_floors_test.py
+++ tests/integration/it_floors_test.py
@@ -57,6 +57,9 @@ def test_create(client, auth):
     assert response.headers["Location"] == "http://localhost/floors/"
     assert response.status_code == HTTPStatus.FOUND
     assert Floor.query.count() == 1
+    floor = Floor.query.first()
+    assert floor.description == floor_data["description"]
+    assert floor.location_id == floor_data["location_id"]
 
 
 @pytest.mark.skip(reason="auth.login() is not yet implemented")
