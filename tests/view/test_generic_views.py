539457ecdcbe87bc22484f5cecc3459ca3f167c7
Vlad Arefev <arefyev@gdeetotdom.ru>
2019-03-12 22:41:15 +0300

    #357: add a few tests

--- tests/view/test_generic_views.py
+++ tests/view/test_generic_views.py
@@ -1,3 +1,5 @@
+import pytest
+
 from timeless.views import ListView
 from timeless.customers.models import Customer
 
@@ -6,10 +8,17 @@ class TestListView(ListView):
     model = Customer
 
 
-def test_view_ordering(app):
+@pytest.mark.parametrize("query_string,sql_query", (
+        ("/?ordering=id,-first_name",
+         "ORDER BY customers.id ASC, customers.first_name DESC"),
+        ("/?ordering=id,+first_name",
+         "ORDER BY customers.id ASC"),
+        ("/?ordering=id,foobar",
+         "ORDER BY customers.id ASC"),
+))
+def test_view_ordering(query_string, sql_query, app):
     view = TestListView()
     query = Customer.query
-    with app.test_request_context("/?ordering=id,-first_name"):
+    with app.test_request_context(query_string):
         query = view.sort_query(query)
-        assert "ORDER BY customers.id ASC, customers.first_name DESC" \
-               in str(query)
+        assert sql_query in str(query)
