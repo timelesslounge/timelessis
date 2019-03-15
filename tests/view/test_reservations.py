96a99055299656fe5def477ee650caa4a4e57dcf
Krzysztof Krason <Krzysztof.Krason@gmail.com>
2019-03-15 15:05:15 +0100

    #379 added sortable table for reservations

--- tests/view/test_reservations.py
+++ tests/view/test_reservations.py
@@ -1,9 +1,9 @@
 """
     Tests for Reservations view.
-    @todo #235:30min Continue implementation of views. Index and a
+    @todo #379:30min Continue implementation of views. Index and a
      view page should be created to list all reservations. In the
      index page there should be also a function to delete the reservation
-     (after confirmation). In the index page it should be possible
-     to sort and filter for every column.
+     (after confirmation). In the index page it should be possible to filter
+     for every column.
 
 """
